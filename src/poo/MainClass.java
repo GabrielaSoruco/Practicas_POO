0ec866dce98b4c7d14d35a16ea43fdc086670fc0
GabrielaSoruco <soruco.gabi@gmail.com>
2021-09-17 19:57:47 -0300

    ingresos de datos desde el teclado

--- src/poo/MainClass.java
+++ src/poo/MainClass.java
@@ -1,20 +1,30 @@
 package src.poo;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Item tuna = new Item();
-        tuna.name = "Atún Gomez";
-        tuna.price = 180.0;
-        tuna.expirationDate = new Date(2021, 8, 12);
+        System.out.println("Introduzca el nombre del item: ");
+        tuna.name = scanner.nextLine();
+        System.out.println("Introduzca el precio: ");
+        tuna.price = scanner.nextDouble();
+        System.out.println("Introuzca el día: ");
+        int day = scanner.nextInt();
+        System.out.println("Introuzca el mes: ");
+        int month = scanner.nextInt();
+        System.out.println("Introuzca el año: ");
+        int year = scanner.nextInt();
+        tuna.expirationDate = new Date(year-1900, month, day);
         tuna.calculateTax();
 
         if (tuna.canItBeSold()){
             System.out.println("Nombre del Item " + tuna.name);
             System.out.println("Precio del Item " + tuna.price);
             System.out.println("Impuesto del Item " + tuna.tax);
-            System.out.println("Total a pagar poor el Item " + tuna.getTotalToPay());
+            System.out.println("Total a pagar por el Item " + tuna.getTotalToPay());
         } else{
             System.out.println("Este ítem no esta habilitado para su venta");
         }
